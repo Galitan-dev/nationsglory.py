bbf725ab930180a56bc40d1d6ada6b03d038d96e
Galitan-dev <galitan.dev@gmail.com>
2022-01-23 18:53:28 +0100

    pypi readme

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 from setuptools import find_packages, setup
 
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     name='nations-glory',
     packages=find_packages(include=['nations_glory']),
-    version='0.1.0',
-    description='A nationsglory.fr Web Scrapper',
+    version='0.1.1',
+    description='A python library that collects information from nationsglory.fr, just for you.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='galitan-dev',
     license='MIT',
     install_requires=['requests==2.22.0', 'html-to-json'],
